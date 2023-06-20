2983dbd3cd98b6a7e6bcb34f8f7beeec206ac85e
Flc゛ <four_leaf_clover@foxmail.com>
2023-06-20 10:55:45 +0800

    refactor: foundation

--- provider.go
+++ provider.go
@@ -2,4 +2,15 @@ package foundation
 
 type Provider interface {
 	Register()
+	Boot()
+	Terminate()
 }
+
+// UnimplementedProvider is a default implementation of the Provider interface.
+type UnimplementedProvider struct{}
+
+func (u *UnimplementedProvider) Register() {}
+
+func (u *UnimplementedProvider) Terminate() {}
+
+func (u *UnimplementedProvider) Boot() {}
